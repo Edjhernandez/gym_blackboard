bf2cb878334636714cedca3003263d55906a2441
Edjhernandez <edjhernandez@gmail.com>
2025-10-30 18:34:41 -0400

    fix: update error handling in LoginScreen and improve styling in InvalidCredentialsModal

--- components/InvalidCredentialsModal.tsx
+++ components/InvalidCredentialsModal.tsx
@@ -23,38 +23,38 @@ const InvalidCredentialsModal: React.FC<InvalidCredentialsModalProps> = ({
       visible={visible}
       onRequestClose={onRetry}
     >
-      <View className="flex-1 justify-center items-center bg-black/50">
-        <View className="w-11/12 max-w-sm bg-gray-900 rounded-lg p-6 items-center">
+      <View className="flex-1 justify-center items-center bg-background-primary">
+        <View className="w-11/12 max-w-sm bg-background-secondary rounded-lg p-6 items-center border-[0.5px] border-text-primary">
           {/* Icon */}
-          <View className="w-20 h-20 bg-yellow-400 rounded-full items-center justify-center border-2 border-black mb-4">
+          <View className="w-20 h-20 bg-primary rounded-full items-center justify-center border-2 border-background-primary mb-4">
             {<ExclamationCircleIcon size={48} color="black" />}
           </View>
 
           {/* Title */}
-          <Text className="text-yellow-400 text-2xl font-bold text-center mb-2">
+          <Text className="text-primary text-2xl font-bold text-center mb-2">
             {t("login.invalid_credentials_popUp.title")}
           </Text>
 
           {/* Message */}
-          <Text className="text-white text-center mb-6">
+          <Text className="text-text-primary text-center mb-6">
             {t("login.invalid_credentials_popUp.message")}
           </Text>
 
           {/* Buttons */}
           <TouchableOpacity
-            className="bg-yellow-400 w-full py-3 rounded-lg mb-3"
+            className="bg-primary w-full py-3 rounded-lg mb-3"
             onPress={onRecover}
           >
-            <Text className="text-black text-center font-bold text-base">
+            <Text className="text-secondary text-center font-bold text-base">
               {t("login.invalid_credentials_popUp.recover_password")}
             </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            className="border border-gray-600 w-full py-3 rounded-lg"
+            className="border border-secondary w-full py-3 rounded-lg"
             onPress={onRetry}
           >
-            <Text className="text-white text-center font-bold text-base">
+            <Text className="text-text-primary text-center font-bold text-base">
               {t("login.invalid_credentials_popUp.try_again")}
             </Text>
           </TouchableOpacity>
