f19171e025c2c392a5316c4ff90b7de558d56ab9
Edjhernandez <edjhernandez@gmail.com>
2025-12-03 12:48:36 -0400

    Add initial project configuration files for IntelliJ IDEA

--- app/(tabs)/blackboard.tsx
+++ app/(tabs)/blackboard.tsx
@@ -1,18 +1,15 @@
-import { useI18n } from "@/lib/hooks/useI18n";
-//import { MaterialCommunityIcons } from "@expo/vector-icons";
+/* import { useI18n } from "@/lib/hooks/useI18n";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-//import { Text, View } from "react-native";
-import useRoutineStore from "@/lib/stores/routineStore";
-import { ScrollView, Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LiveWaitScreen() {
   const { t } = useI18n();
-  const { routine } = useRoutineStore();
-  const zustandState = JSON.stringify(routine, null, 2);
+
   return (
     <SafeAreaView className="flex-1 bg-background-primary justify-center">
-      {/* <View className="flex-1 items-center justify-center px-6">
+      <View className="flex-1 items-center justify-center px-6">
         <MaterialCommunityIcons
           name="television-off"
           size={120}
@@ -23,10 +20,53 @@ export default function LiveWaitScreen() {
         <Text className="mt-6 text-2xl font-semibold text-text-primary text-center">
           {t("blackboard_screen.no_transmission_message")}
         </Text>
-      </View> */}
-      <ScrollView>
-        <Text className="text-green-400">{zustandState}</Text>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
+} */
+
+import React from "react";
+import { CastButton, useRemoteMediaClient } from "react-native-google-cast";
+
+function MyComponent() {
+  // This will automatically rerender when client is connected to a device
+  // (after pressing the button that's rendered below)
+  const client = useRemoteMediaClient();
+  //console.log(CastContext.getPlayServicesState());
+  /*  CastContext.getPlayServicesState().then((state) => {
+    if (state && state !== PlayServicesState.SUCCESS)
+      CastContext.showPlayServicesErrorDialog(state);
+  }); */
+
+  if (client) {
+    // Send the media to your Cast device as soon as we connect to a device
+    // (though you'll probably want to call this later once user clicks on a video or something)
+    client.loadMedia({
+      mediaInfo: {
+        contentUrl:
+          "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4",
+        contentType: "video/mp4",
+        metadata: {
+          images: [
+            {
+              url: "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/images/480x270/BigBuckBunny.jpg",
+            },
+          ],
+          title: "Big Buck Bunny",
+          subtitle:
+            "A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.",
+          studio: "Blender Foundation",
+          type: "movie",
+        },
+        streamDuration: 596, // seconds
+      },
+      startTime: 10, // seconds
+    });
+  }
+
+  // This will render native Cast button.
+  // When a user presses it, a Cast dialog will prompt them to select a Cast device to connect to.
+  return <CastButton style={{ width: 24, height: 24, tintColor: "black" }} />;
 }
+
+export default MyComponent;
