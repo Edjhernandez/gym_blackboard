89c42fe1c90a9f1cbe27735b16a2749fab0f7afa
Edjhernandez <edjhernandez@gmail.com>
2025-12-06 15:56:26 -0400

    feat: enhance MyComponent with Google Cast functionality and improve styling

--- app/(tabs)/blackboard.tsx
+++ app/(tabs)/blackboard.tsx
@@ -28,6 +28,7 @@ export default function LiveWaitScreen() {
 import React from "react";
 import { Pressable, Text } from "react-native";
 import { CastButton, useRemoteMediaClient } from "react-native-google-cast";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function MyComponent() {
@@ -38,8 +39,8 @@ function MyComponent() {
   /*  CastContext.getPlayServicesState().then((state) => {
     if (state && state !== PlayServicesState.SUCCESS)
       CastContext.showPlayServicesErrorDialog(state);
-  }); */
-
+  });  */
+  console.log("client", client);
   if (client) {
     // Send the media to your Cast device as soon as we connect to a device
     // (though you'll probably want to call this later once user clicks on a video or something)
@@ -66,17 +67,15 @@ function MyComponent() {
     });
   }
 
-  // This will render native Cast button.
-  // When a user presses it, a Cast dialog will prompt them to select a Cast device to connect to.
   return (
-    <SafeAreaView className="flex-1 bg-white justify-center items-center">
-      <Text className="mt-20">Google Cast Button:</Text>
+    <SafeAreaView className="flex-1 bg-background-primary justify-center items-center">
+      <Text className="mt-20 text-text-primary">Google Cast Button:</Text>
       <Pressable onPress={() => console.log("Cast button pressed")}>
         <CastButton
           style={{
             width: 24,
             height: 24,
-            tintColor: "black",
+            tintColor: "#E7EBDA",
             marginBottom: 50,
           }}
         />
