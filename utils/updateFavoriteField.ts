ec7fcf2d186622d82086405ad7406d18aca51028
Edjhernandez <edjhernandez@gmail.com>
2025-12-06 12:47:21 -0400

    feat: remove commented code and improve clarity in updateFavoriteField function

--- utils/updateFavoriteField.ts
+++ utils/updateFavoriteField.ts
@@ -5,12 +5,9 @@ export const updateFavoriteField = async (
   id: string,
   newFieldValue: boolean
 ) => {
-  // 1. Crea una referencia al documento específico de la rutina
-  // 'rutinas' es el nombre de tu colección
   const routineRef = doc(db, "routines", id);
 
   try {
-    // 2. Usa updateDoc para cambiar el campo 'esFavorita'
     await updateDoc(routineRef, {
       isFavorite: newFieldValue,
     });
