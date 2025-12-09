41c25cb5cb7202c1bbaf71581c61b9906735448e
Edjhernandez <edjhernandez@gmail.com>
2025-12-08 22:56:14 -0400

    feat: integrate Google Cast functionality and update receiver app ID

--- app/(tabs)/blackboard.tsx
+++ app/(tabs)/blackboard.tsx
@@ -27,58 +27,44 @@ export default function LiveWaitScreen() {
 
 import React from "react";
 import { Pressable, Text } from "react-native";
-import { CastButton, useRemoteMediaClient } from "react-native-google-cast";
-
+import {
+  CastButton,
+  useCastChannel,
+  useCastSession,
+  useCastState,
+  useDevices,
+  useRemoteMediaClient,
+} from "react-native-google-cast";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function MyComponent() {
-  // This will automatically rerender when client is connected to a device
-  // (after pressing the button that's rendered below)
   const client = useRemoteMediaClient();
-  //console.log(CastContext.getPlayServicesState());
-  /*  CastContext.getPlayServicesState().then((state) => {
-    if (state && state !== PlayServicesState.SUCCESS)
-      CastContext.showPlayServicesErrorDialog(state);
-  });  */
-  console.log("client", client);
-  if (client) {
-    // Send the media to your Cast device as soon as we connect to a device
-    // (though you'll probably want to call this later once user clicks on a video or something)
-    client.loadMedia({
-      mediaInfo: {
-        contentUrl:
-          "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4",
-        contentType: "video/mp4",
-        metadata: {
-          images: [
-            {
-              url: "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/images/480x270/BigBuckBunny.jpg",
-            },
-          ],
-          title: "Big Buck Bunny",
-          subtitle:
-            "A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.",
-          studio: "Blender Foundation",
-          type: "movie",
-        },
-        streamDuration: 596, // seconds
-      },
-      startTime: 10, // seconds
-    });
-  }
+  const session = useCastSession();
+  const channel = useCastChannel("urn:x-cast:com.google.cast.demo.helloworld");
+  const devices = useDevices();
+  const castState = useCastState();
+
+  const handlePressable = async () => {
+    if (client) {
+      //await channel?.sendMessage({ greeting: "Hola Mundo" });
+    }
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-background-primary justify-center items-center">
       <Text className="mt-20 text-text-primary">Google Cast Button:</Text>
-      <Pressable onPress={() => console.log("Cast button pressed")}>
-        <CastButton
-          style={{
-            width: 24,
-            height: 24,
-            tintColor: "#E7EBDA",
-            marginBottom: 50,
-          }}
-        />
+      <CastButton
+        style={{
+          width: 24,
+          height: 24,
+          tintColor: "#E7EBDA",
+          marginBottom: 50,
+        }}
+      />
+      <Pressable onPress={handlePressable}>
+        <Text className="text-text-primary underline">
+          Enviar "Hola Mundo" al Chromecast
+        </Text>
       </Pressable>
     </SafeAreaView>
   );
